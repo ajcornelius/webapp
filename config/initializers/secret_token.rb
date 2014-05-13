e4a863c69c87ba59d6d402adbd48f7b7307d4a5e
cornelius <ajcornelius@gmail.com>
2014-05-13 19:08:18 +0000

    Adjust production.rb line27 so it compiles assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Queerstir::Application.config.secret_key_base = 'cfa39300283dfcac85b9eff089298f19151a4562872fff2d640897d09aa5a7ec44f5554a2709d61b9b5a5c8df8d848770276cf55bd44454172a315895ed405da'
