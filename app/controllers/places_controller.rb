691c249bee6a1ef2cf639e2dd957b3f5467b3cec
cornelius <ajcornelius@gmail.com>
2014-06-01 03:46:35 +0000

    Made submit function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,18 @@ class PlacesController < ApplicationController
   	@places = Place.order("name").page(params[:page]).per(5)
 end
 
-  	def new
+  def new
   		@place = Place.new
+  end
+
+  def create
+  	Place.create(place_params)
+  	redirect_to root_path
+  end
+
+  private
+
+  def place_params
+  	params.require(:place).permit(:name, :description, :address)
   end
 end
