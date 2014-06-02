0900d022e3fe674ef4cc0d040dd27c4912a00271
cornelius <ajcornelius@gmail.com>
2014-06-02 05:47:23 +0000

    added error and check to forms for add place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ end
   end
 
   def create
-  	current_user.places.create(place_params)
-  	redirect_to root_path
+  	 @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, :status => :unprocessable_entity
+  end
   end
 
   def show
@@ -31,7 +35,11 @@ end
   end
   
     @place.update_attributes(place_params)
+     if @place.valid?
     redirect_to root_path
+  else
+    render :edit, :status => :unprocessable_entity
+  end
   end
 
   def destroy
